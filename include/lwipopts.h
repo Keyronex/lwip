7eb713011597a407b9e86dca3ca182e706762d8c
NetBSD User <netbsduser@users.noreply.github.com>
2023-01-26 10:49:34 +0000

    lwipopts: reorganise

--- include/lwipopts.h
+++ include/lwipopts.h
@@ -7,17 +7,18 @@
 #undef errno
 #define errno curcpu()->running_thread->errno
 
-#define LWIP_DHCP 1
 #if 0
 #define LWIP_DEBUG 1
 #define TCP_DEBUG                       LWIP_DBG_ON
 #define ETHARP_DEBUG                    LWIP_DBG_ON
-#define PBUF_DEBUG                      LWIP_DBG_ON
+//#define PBUF_DEBUG                      LWIP_DBG_ON
 #define IP_DEBUG                        LWIP_DBG_ON
 #define TCPIP_DEBUG                     LWIP_DBG_ON
 #define DHCP_DEBUG                      LWIP_DBG_ON
 #define UDP_DEBUG                       LWIP_DBG_ON
 #endif
+
+#define LWIP_DHCP 1
 #define LWIP_DNS 1
 #define LWIP_SOCKET 1
 #define LWIP_TIMEVAL_PRIVATE 0
